1e4f9d7a865dfb55138f54893ac821b26eb35f67
jeremy woo <kepungmoin@hotmail.com>
2019-07-16 21:04:49 +1200

    clse result

--- dotnet/key/KeyApp/Program.cs
+++ dotnet/key/KeyApp/Program.cs
@@ -9,67 +9,103 @@ using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Microsoft.Azure;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Azure.KeyVault.WebKey;
+using System.Net.Http;
+using static Microsoft.Azure.KeyVault.KeyVaultClient;
 
 namespace KeyApp
 {
     class Program
     {
+        static ClientCredential clientCred = null;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
             var client = new KeyClient(vaultUri: new Uri("https://wickedvault.vault.azure.net/"), credential: new DefaultAzureCredential());
-
         
-            var key = client.GetKey("masterKey");            
-                        
+            var key = client.GetKey("masterKey"); 
             System.Console.WriteLine(key.Value.Name);
-
             System.Console.WriteLine(key.Value.KeyMaterial.KeyType);
 
             string value = "testing";
 
             var encodedText = Encrypt(key, value);
-
-            Decrypt(key, encodedText).GetAwaiter().GetResult();
-
+            Decrypt(encodedText).GetAwaiter().GetResult();
         }
 
-        private async static Task<string> GetToken(string authority, string resource, string scope)
-        {
-            var authContext = new AuthenticationContext(authority);
-            ClientCredential clientCred = new ClientCredential(
-                "5314007c-a783-4b92-b713-e928214947e9",
-                "0f76365a-7abc-40c5-b8f1-05e6964dae6c");
-            AuthenticationResult result = await authContext.AcquireTokenAsync(resource, clientCred);
-
-            if (result == null)
-                throw new InvalidOperationException("Failed to obtain the JWT token");
-
-            return result.AccessToken;
-        }
-
-
-        private static async Task<string> Decrypt(Azure.Response<Key> key, string encryptedText) 
-        {                  
-
+        private static async Task<string> Decrypt(string encryptedText) 
+        {       
             var encryptedBytes = Convert.FromBase64String(encryptedText);
+            System.Console.WriteLine("Decypting text");
+            System.Console.WriteLine(encryptedBytes);
+    
+    	// var clientCredential = new ClientCredential("5314007c-a783-4b92-b713-e928214947e9", "0f76365a-7abc-40c5-b8f1-05e6964dae6c");
+        // var kvc = new KeyVaultCredential(GetAccessTokenAsync);
+    
+            //GetKeyContent("https://wickedvault.vault.azure.net/");
 
-                       var azureServiceTokenProvider = new AzureServiceTokenProvider();
-                       var keyVaultClient = new KeyVaultClient(
-                           new KeyVaultClient.AuthenticationCallback(
-                               azureServiceTokenProvider.KeyVaultTokenCallback));
-                     //  builder.AddAzureKeyVault(
-                     //      keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+            //var kv = GetKeyVaultClient(GetKeyVaultCallback());
+        
 
+            // var azureServiceTokenProvider = new AzureServiceTokenProvider();
+
+            ///var kv = new KeyVaultClient(kvc, GetHttpClient());
+            //var sc = kv.GetSecretAsync("https://wickedvault.vault.azure.net/", "masterKey");
             
-            var decryptionResult = await keyVaultClient.EncryptAsync("masterKey",  JsonWebKeyEncryptionAlgorithm.RSAOAEP, encryptedBytes);
+
+            AuthenticationCallback callback = async (authority,resource,scope) =>
+            {
+                var appId = "5314007c-a783-4b92-b713-e928214947e9";
+                var appSecret = "0f76365a-7abc-40c5-b8f1-05e6964dae6c";
+
+                System.Console.WriteLine($"Authority {authority}");
+                System.Console.WriteLine($"resource {resource}");
+                System.Console.WriteLine($"scope {scope}");
+
+                var authContext = new AuthenticationContext(authority);
+                
+                var credential = new ClientCredential(appId, appSecret);
+                var authResult = await authContext.AcquireTokenAsync(resource, credential);
+                return authResult.AccessToken;
+            };
+            
+            System.Console.WriteLine($"Setting up client");
+            var client = new KeyVaultClient(callback);  
+
+            var result = client.GetSecretAsync("https://wickedvault.vault.azure.net", "test");   
+
+            Console.WriteLine(result.Id);
+            Console.WriteLine(client.ApiVersion);
+
+            var decryptionResult = await client.DecryptAsync("masterKey",  JsonWebKeyEncryptionAlgorithm.RSAOAEP, encryptedBytes);
 
              var decryptedText = Encoding.Unicode.GetString(decryptionResult.Result);
 
             return decryptedText;
 
-        }                
+        }         
+
+
+        private static HttpClient GetHttpClient()
+        {
+            var http = new HttpClient();
+            //http.BaseAddress = new Uri("https://wickedvault.vault.azure.net");
+            return http;
+        }       
+
+        private static async Task<string> GetAccessTokenAsync(string authority, string resource, string scope)
+        {
+            System.Console.WriteLine(authority);
+            System.Console.WriteLine(resource);
+            System.Console.WriteLine(scope);
+
+            var context = new AuthenticationContext(authority, TokenCache.DefaultShared);
+            var result = await context.AcquireTokenAsync(resource, clientCred);
+            Console.WriteLine(scope);
+            return result.AccessToken;
+        }
+
 
         private static string Encrypt(Azure.Response<Key> key, string value) {
 
@@ -92,17 +128,28 @@ namespace KeyApp
 
                 return encodedText;            
             }                   
-
         }
+
+        private static AzureServiceTokenProvider.TokenCallback GetKeyVaultCallback()
+        {
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            return azureServiceTokenProvider.KeyVaultTokenCallback;
+        }
+
+        private static KeyVaultClient GetKeyVaultClient(AzureServiceTokenProvider.TokenCallback callback)
+        {
+            KeyVaultClient.AuthenticationCallback authCallback = new KeyVaultClient.AuthenticationCallback(callback);
+            return new KeyVaultClient(authCallback);
+        }
+
+        public static string GetKeyContent(string keyIdentifier)
+        {
+            var kv = GetKeyVaultClient(GetKeyVaultCallback());
+            var key = kv.GetKeyAsync(keyIdentifier);
+            return key.Result.Key.ToString();
+        }
+
     }
 }
 
 
-
-// {
-//   "appId": "5314007c-a783-4b92-b713-e928214947e9",
-//   "displayName": "wickedapp",
-//   "name": "http://wickedapp",
-//   "password": "0f76365a-7abc-40c5-b8f1-05e6964dae6c",
-//   "tenant": "c7edfba3-a241-4b80-a92f-a311dc6cd2df"
-// }
